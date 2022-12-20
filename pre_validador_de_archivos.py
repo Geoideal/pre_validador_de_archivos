e1c8904345c531e180c69e47edbb66f9a597442c
Germán Carrillo <gcarrillo@linuxmail.org>
2022-12-20 17:00:01 +0100

    Add optional parameter model_to_validate; test that model_to_validate is in data (XTF/GPKG) models

--- pre_validador_de_archivos.py
+++ pre_validador_de_archivos.py
@@ -8,26 +8,29 @@ import sqlite3
 from pre_validador_utils import (get_models_from_xtf,
                                  get_gpkg_models)
 
+LEV_CAT_1_2 = 'Modelo_Aplicacion_LADMCOL_Lev_Cat_V1_2'
+RIC_0_1 = 'Modelo_Aplicacion_LADMCOL_RIC_V0_1'
+
 # At least one of these is required!
-LADMCOL_MODEL_NAMES = ['Modelo_Aplicacion_LADMCOL_Lev_Cat_V1_2', 'Modelo_Aplicacion_LADMCOL_RIC_V0_1']
+LADMCOL_MODEL_NAMES = [LEV_CAT_1_2, RIC_0_1]
 
 
-def pre_validar_archivo(path):
+def pre_validar_archivo(path, model_to_validate=''):
     if not os.path.exists(path):
         return False, "El archivo no existe!"
 
     extension = __get_extension(path)
 
     if extension == '.zip':
-        return pre_validar_zip(path)
+        return pre_validar_zip(path, model_to_validate)
     elif extension == '.xtf':
-        return pre_validar_xtf(path)
+        return pre_validar_xtf(path, model_to_validate)
     elif extension == '.gpkg':
-        return pre_validar_gpkg(path)
+        return pre_validar_gpkg(path, model_to_validate)
     else:
         return False, "Extensión de archivo inválida ('{}').".format(extension)
         
-def pre_validar_zip(path):
+def pre_validar_zip(path, model_to_validate=''):
     # Tiene un solo archivo dentro?
     # El archivo tiene extensión válida?
     # Permite descomprimir?
@@ -50,7 +53,7 @@ def pre_validar_zip(path):
     except zipfile.BadZipFile as e:
         return False, "Problema al descomprimir el archivo ZIP ('{}')! Detalle: {}".format(path, e)
         
-    res, msg = pre_validar_archivo(os.path.join(tmp_dir, in_files[0]))
+    res, msg = pre_validar_archivo(os.path.join(tmp_dir, in_files[0]), model_to_validate)
     try:
         shutil.rmtree(tmp_dir)  # Remove the directory and its file
     except:
@@ -58,7 +61,7 @@ def pre_validar_zip(path):
     
     return res, msg
         
-def pre_validar_xtf(path):
+def pre_validar_xtf(path, model_to_validate=''):
     # XML?
     # Tags requeridos?
     # Modelos requeridos?
@@ -72,13 +75,16 @@ def pre_validar_xtf(path):
                 count += 1
 
         if count == len(LADMCOL_MODEL_NAMES):
-            return False, "¡El archivo XTF no incluye el modelo LADM-COL requerido ('{}')!".format("', '".join(LADMCOL_MODEL_NAMES))
+            return False, "¡El archivo XTF no incluye ninguno de los modelos LADM-COL requeridos ('{}')!".format("', '".join(LADMCOL_MODEL_NAMES))
+        
+        if model_to_validate and not model_to_validate in models:
+            return False, "¡El archivo XTF no incluye el modelo LADM-COL a validar ('{}')!".format(model_to_validate)
     else:
         return res, models
     
     return True, "Archivo pre-válido!"
 
-def pre_validar_gpkg(path):
+def pre_validar_gpkg(path, model_to_validate=''):
     # Es un GPKG?
     # Tiene tablas de metadatos de ili2db o coinciden los nombres de tablas?
     conn = sqlite3.connect(path)
@@ -111,7 +117,11 @@ def pre_validar_gpkg(path):
                 count += 1
 
         if count == len(LADMCOL_MODEL_NAMES):
-            return False, "¡La BD GeoPackage no incluye el modelo LADM-COL requerido ('{}')!".format("', '".join(LADMCOL_MODEL_NAMES))
+            return False, "¡La BD GeoPackage no incluye ninguno de los modelos LADM-COL requeridos ('{}')!".format("', '".join(LADMCOL_MODEL_NAMES))
+
+        if model_to_validate and not model_to_validate in models:
+            return False, "¡El archivo XTF no incluye el modelo LADM-COL a validar ('{}')!".format(model_to_validate)
+
         #print(c.fetchone())
     except:
         return False, "Problema accediendo a la GPKG."
@@ -131,16 +141,16 @@ if __name__ == '__main__':
     def abs_path(rel_path):
         os.path.join(pre_path, rel_path)
     
-    def do_assert(archivo):
-        res, msg = pre_validar_archivo(archivo)
+    def do_assert(archivo, model_to_validate=''):
+        res, msg = pre_validar_archivo(archivo, model_to_validate)
         print(msg)
         return res
     
-    def assert_true(archivo):
-        assert do_assert(archivo)
+    def assert_true(archivo, model_to_validate=''):
+        assert do_assert(archivo, model_to_validate)
     
-    def assert_false(archivo):
-        assert not do_assert(archivo)
+    def assert_false(archivo, model_to_validate=''):
+        assert not do_assert(archivo, model_to_validate)
 
     # -------------------ZIP------------------------
     print("INFO: Probando ZIPs...")
@@ -150,6 +160,8 @@ if __name__ == '__main__':
     assert_false('data/zip/archivo_de_texto_xtf.zip')
     assert_false('data/zip/datos_de_prueba_lev_cat_1_0.zip')
     assert_true('data/zip/lev_cat_1_2_valido_01.zip')
+    assert_true('data/zip/lev_cat_1_2_valido_01.zip', LEV_CAT_1_2)
+    assert_false('data/zip/lev_cat_1_2_valido_01.zip', RIC_0_1)
 
     # -------------------XTF------------------------
     print("\nINFO: Probando XTFs...")
@@ -161,8 +173,10 @@ if __name__ == '__main__':
     assert_false('data/xtf/archivo_de_texto.xtf')
     assert_false('data/xtf/ilivalidator_errors.xtf')
     assert_true('data/xtf/lev_cat_1_2_valido_01.xtf')
-    assert_true('data/xtf/lev_cat_1_2_invalido_01.xtf')
     assert_false('data/xtf/datos_de_prueba_lev_cat_1_0.xtf')
+    assert_true('data/xtf/lev_cat_1_2_invalido_01.xtf')
+    assert_true('data/xtf/lev_cat_1_2_invalido_01.xtf', LEV_CAT_1_2)
+    assert_false('data/xtf/lev_cat_1_2_invalido_01.xtf', RIC_0_1)
 
     # -------------------GPKG------------------------
     print("\nINFO: Probando GPKGs...")
@@ -172,5 +186,7 @@ if __name__ == '__main__':
     assert_false('data/gpkg/interlis_no_modelo.gpkg')
     assert_false('data/gpkg/valida_1_0.gpkg')
     assert_true('data/gpkg/valida_1_2.gpkg')
+    assert_true('data/gpkg/valida_1_2.gpkg', LEV_CAT_1_2)
+    assert_false('data/gpkg/valida_1_2.gpkg', RIC_0_1)
    
     print('\nAll tests passed!')
